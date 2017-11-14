b0659421747eaadeb437a31e37a5fa0814ecfe0f
kaiwen <dengkaiwen@xhqb.com>
2017-11-14 17:03:05 +0800

    vars

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -13,23 +13,23 @@ def call(body) {
                 stage ('Clone') {
                     checkout scm
                 }
-                stage ('Build') {
-                    sh "echo 'building ${config.projectName} ...'"
-                }
-                stage ('Tests') {
-                    parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
-                    },
-                    'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
-                    },
-                    'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                }
-                stage ('Deploy') {
-                    sh "echo 'deploying to server ${config.serverDomain}...'"
-                }
+               // stage ('Build') {
+               //     sh "echo 'building ${config.projectName} ...'"
+               // }
+               // stage ('Tests') {
+               //     parallel 'static': {
+              //          sh "echo 'shell scripts to run static tests...'"
+               //     },
+                //    'unit': {
+               //         sh "echo 'shell scripts to run unit tests...'"
+               //     },
+              //      'integration': {
+             //           sh "echo 'shell scripts to run integration tests...'"
+            // //       }
+            //    }
+              //  stage ('Deploy') {
+             //       sh "echo 'deploying to server ${config.serverDomain}...'"
+           //     }
             } catch (err) {
                 currentBuild.result = 'FAILED'
                 throw err
