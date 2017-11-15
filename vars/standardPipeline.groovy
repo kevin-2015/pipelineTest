a2f5b2bce35af5bfb4e4b2ac444c6e82c5788cd0
kaiwen <dengkaiwen@xhqb.com>
2017-11-15 16:24:06 +0800

    modified

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -6,33 +6,18 @@ def call(body) {
         body()
 
         node {
-            // Clean workspace before doing anything
-            deleteDir()
-
-            try {
-                stage ('Clone') {
-                    checkout scm
-                }
-               // stage ('Build') {
-               //     sh "echo 'building ${config.projectName} ...'"
-               // }
-               // stage ('Tests') {
-               //     parallel 'static': {
-              //          sh "echo 'shell scripts to run static tests...'"
-               //     },
-                //    'unit': {
-               //         sh "echo 'shell scripts to run unit tests...'"
-               //     },
-              //      'integration': {
-             //           sh "echo 'shell scripts to run integration tests...'"
-            // //       }
-            //    }
-              //  stage ('Deploy') {
-             //       sh "echo 'deploying to server ${config.serverDomain}...'"
-           //     }
-            } catch (err) {
-                currentBuild.result = 'FAILED'
-                throw err
-            }
+           timestamps {
+				stage("scm checkout") {                
+					checkout scm
+				}
+		   }
+		   
+		   //def buildSteps = new BaseSteps(this, config)
+           // def envName = "build"
+          //  for (def name : buildSteps.stageNames()) {
+          //      stage(name) {
+          //          buildSteps.stepOfStage(name)()
+         //       }
+         //   }
         }
     }
