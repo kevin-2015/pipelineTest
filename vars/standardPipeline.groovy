6c72b0d711fc4c69ccfb86657a1e2c663ffe777b
kaiwen <dengkaiwen@xhqb.com>
2017-12-21 11:10:05 +0800

    first commit

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -1,28 +1,30 @@
 def call(body) {
 
-        def config = [:]
-        body.resolveStrategy = Closure.DELEGATE_FIRST
-        body.delegate = config
-        body()
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
-        node {
-           timestamps {
-			stage("scm checkout") {                
-				checkout scm
-			}
-			
-			 stage("build") {
-                def gradle_command = "gradle --quiet clean build";
-                steps.echo gradle_command
-                //在指定目录下执行命令  
-                steps.dir('MyApplication/') {
-                  //steps. gradle_command
-				  bat 'gradlew assembleDebug'
-                }   
-           }
-		   
-			
-		   }
+    node {
+       timestamps {
+	       stage("scm checkout") {                
+		        checkout scm
+         }
+
+	
+      	 stage("build") {
+            def gradle_command = "gradle --quiet clean build";
+            steps.echo gradle_command
+            //在指定目录下执行命令  
+            steps.dir('MyApplication/') {
+              steps.sh "gradle --quiet clean build"
+		           // bat 'gradlew assembleDebug'
+            }   
+         }
+      }
+   
+	
+   }
 		  
 		  
 		   
